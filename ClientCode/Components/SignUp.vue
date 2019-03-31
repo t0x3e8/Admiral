60cbc2caf091f1e51e2ae61107c1e26ed5995f13
Jarek <t0x3e8@gmail.com>
2019-03-31 13:46:14 +0200

    refactor of Signup view

--- ClientCode/Components/SignUp.vue
+++ ClientCode/Components/SignUp.vue
@@ -4,54 +4,51 @@
       v-model="playerName"
       :rules="nameRules"
       :counter="10"
-      label="Your name"
+      label="Your nickname"
       required
     ></v-text-field>
-    <v-btn
-      :disabled="!valid"
-      @click="join"
-    >
-      Join
-    </v-btn>
-    <v-btn @click="clear">Clear</v-btn>
+    <v-btn :disabled="!valid" @click="join">Join game</v-btn>
   </v-form>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    components: {},
-    data() {
-        return {
-        playerName: '',
-        valid: false,
-        counter: 10,
-        nameRules: [
-            v => Boolean(v) || 'A player must have a name',
-            v => v.length <= this.counter || 'Player\'s name should not be too long',
-            v => (/^[a-z](?:_?[ a-z0-9]+)*$/iu).test(v) || 'Player\'s name must start with alphabetical letters'
-        ]
-        };
-    },
-    computed: {},
-    methods: {
+  components: {},
+  data() {
+    return {
+      playerName: "",
+      valid: false,
+      counter: 10,
+      nameRules: [
+        v => Boolean(v) || "Enter your nickname",
+        v =>
+          v.length <= this.counter ||
+          "Really? The nickname must not be that long.",
+        v =>
+          /^[a-z](?:_?[ a-z0-9]+)*$/iu.test(v) ||
+          "Really? Enter a human nickname."
+      ]
+    };
+  },
+  computed: {},
+  methods: {
     join() {
-        axios.post(
-            '/user/CreateJWTToken',
-            {
-                'username': this.playerName
-            }
-        )
-        .then(response => {
-            this.playerName = `your names has been submitted to the server ${response}`;
+      var that = this;
+      axios
+        .post("/user/CreateJWTToken", {
+          username: this.playerName
         })
-        .then(error => {
-            this.playerName = `error ${error}`;
+        .then(function(response) {
+          that.playerName = `TODO: your names has been submitted to the server ${response}`;
+        })
+        .catch(function (error) {
+          that.playerName = `error ${error}`;
         });
     },
     clear() {
-      this.playerName = '';
+      this.playerName = "";
     }
   }
 };
