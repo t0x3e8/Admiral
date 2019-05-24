2597f36c64aa8bd996b271e0bf278d22798d3fb1
Jarek <t0x3e8@gmail.com>
2019-05-24 21:58:47 +0200

    move of App.vue under Componenents

---
+++ ClientCode/Components/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <sign-up />
+  </div>
+</template>
+
+<script>
+  import SignUp from './SignUp.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      SignUp
+    }
+  };
+</script>
+
+<style>
+</style>
--- ClientCode/main.js
+++ ClientCode/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './Components/App.vue'
 import Vuetify from 'vuetify'
 
 Vue.use(Vuetify)
